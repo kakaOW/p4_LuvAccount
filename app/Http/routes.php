668ba20dd9389a997f8c1261f0505f7d9cd736b1
Yung Wei <kaka@owstartup.com>
2016-04-22 19:19:37 +0800

    Edit Route.php plan

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,35 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+#Authentication
+Route::get('/login', 'Auth\AuthController@getLogin');
+Route::post('/login', 'Auth\AuthController@postLogin');
+Route::get('/register', 'Auth\AuthController@getRegister');
+Route::post('/register', 'Auth\AuthController@postRegister');
+Route::get('/logout', 'Auth\AuthController@logout');
+Route::get('/show-login-status', function() {
+    # You may access the authenticated user via the Auth facade
+    $user = Auth::user();
+    if($user) {
+        echo 'You are logged in.';
+        dump($user->toArray());
+    } else {
+        echo 'You are not logged in.';
+    }
+    return;
 });
+
+#List specific routes
+
+//Home
+Route::get('/list', 'RandomUserController@getIndex');
+
+//Create
+Route::get('/list/create', 'LoremIpsumController@getCreate');
+Route::post('/list/ceate', 'LoremIpsumController@postCreate');
+
+//Edit
+Route::get('/list/{id?}', 'RandomUserController@getEdit');
+Route::post('/list/{id?}', 'RandomUserController@postEdit');
+
+//Show
+Route::get('/list/{id?}', 'RandomUserController@getShow');
