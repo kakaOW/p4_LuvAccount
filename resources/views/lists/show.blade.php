c6dc2f29417999e79c5a73d95842c044dae9e7a3
Yung Wei <kaka@owstartup.com>
2016-05-12 20:39:16 +0800

    fix show page

--- resources/views/lists/show.blade.php
+++ resources/views/lists/show.blade.php
@@ -80,14 +80,14 @@
             </div>
             <div class="small-12 columns">
                 <i class="fa fa-plus" aria-hidden="true">
-                @if(isset($entries->list_id))
+                @if(strval($entries) != "[]")
                 <?php $i= 0 ; foreach($entries as $entry){if($entry->points > 0){$temp[$i]=$entry->points; $i++;};} echo array_sum($temp);?>
                 @else
                 0
                 @endif
                 </i>
                 <i class="fa fa-minus" aria-hidden="true">
-                @if(isset($entries->list_id))
+                @if(strval($entries) != "[]")
                 <?php $a= 0 ; foreach($entries as $entry){if($entry->points < 0){$temp2[$a]=$entry->points; $a++;};} echo -array_sum($temp2);?>
                 @else
                 0
